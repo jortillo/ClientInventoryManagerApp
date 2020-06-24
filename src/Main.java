6f25cd8735c6ef24998d34778c27a12de02d635e
Jeric Ortillo <33136669+jortillo@users.noreply.github.com>
2020-06-23 22:58:43 -0700

    Added Initial GUI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import gui.*;
+import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("This main class works!");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Starter starter = new Starter();
+            }
+        });
     }
 }
---
+++ src/gui/Starter.java
@@ -0,0 +1,12 @@
+package gui;
+import java.awt.*;
+import javax.swing.*;
+
+public class Starter extends JFrame{
+    public Starter(){
+        setTitle("Client Inventory Manager");
+        setSize(900,900);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
